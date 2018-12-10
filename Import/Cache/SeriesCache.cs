e2c2e7118f6d2af7cee1797c9419e561857c9049
ksandr <alex@ksandr.net>
2018-12-10 11:58:59 +0300

    Импорт архива библиотеки

---
+++ Import/Cache/SeriesCache.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ksandr.Books.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ksandr.Books.Import.Cache
+{
+    internal class SeriesCache
+    {
+        private readonly BooksContext _db;
+        private readonly Dictionary<string, Series> _cache;
+
+        public SeriesCache(BooksContext db)
+        {
+            _db = db;
+            _cache = new Dictionary<string, Series>();
+        }
+
+        public async Task<Series> GetAsync(string title)
+        {
+            string searchTitle = title.ToUpper();
+
+            if (_cache.TryGetValue(searchTitle, out Series series))
+                return series;
+
+
+            series = await _db.Series.FirstOrDefaultAsync(x => x.SearchTitle == searchTitle);
+            if (series == null)
+            {
+                series = new Series()
+                {
+                    Title = title,
+                    SearchTitle = searchTitle
+                };
+
+                _db.Add(series);
+            }
+
+            _cache.Add(searchTitle, series);
+
+            return series;
+        }
+    }
+}
---
+++ Import/InpxEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Ksandr.Books.Import
+{
+    public class InpxEntry
+    {
+        public InpxEntry(string name, Stream entry)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException(nameof(name));
+            if (entry == null)
+                throw new ArgumentNullException(nameof(entry));
+
+            Name = name;
+            Entry = entry;
+        }
+
+        public string Name { get; private set; }
+        public Stream Entry { get; private set; }
+    }
+}
